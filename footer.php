48beb9522dd4b5d284b3bf49819761188fc23ed3
Jacob Duvander <duvan.der@live.se>
2019-10-02 19:31:10 +0200

    Centered mobile menu and fixed image linking in footer

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
                     <a href="email"><i class="far fa-envelope"></i></a>
                 </div>
                 <div class="logo-wrapper-foot">
-                    <img src="http://localhost/AxelTidehorn/wordpress/wp-content/uploads/2019/08/LogoTxtW.png" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo/LogoTxtW.svg" />
                 </div>
                 <div class="maker">
                     <small>Axel Tidehorn, <?php echo date("Y"); ?></small>
