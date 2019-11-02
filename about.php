546fa1f6b5a552e597e935c8fe2a73716de9fafa
Axel Tidehorn <axel.tidehorn.96@gmail.com>
2019-11-02 18:11:46 +0100

    Mjaadu

--- about.php
+++ about.php
@@ -9,6 +9,12 @@
         <p>
             Ordet fotografi kommer från grekiskans phōs och graphé som översätts till ljus och skriva. Och om man lägger till tid så är det exakt det jag som fotograf gör. Skriver, berättar, genom att fånga ljus och tid i ögonblick.
         </p>
+        <div class="portrait-wrapper">
+            <div class="portrait-overlay">
+                <img src="http://localhost/AxelTidehorn2/wordpress/wp-content/uploads/2019/10/IMG_8996.jpg" alt="">
+            </div>
+
+        </div>
         <p>
             Mitt namn är Axel Tidehorn och jag är en Jönköpingsbaserad fotograf vars mål är att fånga och berätta Er historia. Oavsett om det är Ert företag, produkt, familj, husdjur eller Er stora dag.
             Era önskningar och mål är det viktigaste för mig och jag tror att vi tillsammans kan fånga ljuset och skapa något magiskt!
