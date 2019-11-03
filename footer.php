a3b3a92882d70ad142d995f29c3a4f2e1b9ea4cb
Axel Tidehorn <axel.tidehorn.96@gmail.com>
2019-11-03 11:41:46 +0100

    Instagram

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="footer-wrapper">
                 <div class="social-wrapper">
-                    <a SameSite=secure target="_blank" href="https://www.instagram.com/axeltidehorn/"><i class="fab fa-instagram"></i></a>
+                    <a SameSite=secure target="_blank" href="https://www.instagram.com/axeltidehornphotography/"><i class="fab fa-instagram"></i></a>
                     <a SameSite=secure target="_blank" href="https://www.behance.net/axeltideho06f3"><i class="fab fa-behance"></i></a>
                     <a SameSite=secure target="_blank" href="https://www.linkedin.com/in/axeltidehorn/"><i class="fab fa-linkedin-in"></i></a>
                     <a href="mailto:hej@axeltidehorn.se"><i class="far fa-envelope"></i></a>
