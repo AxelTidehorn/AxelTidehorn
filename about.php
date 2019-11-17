271cd433b2746b71d786fd0392369d093fcd9c70
Axel Tidehorn <axel.tidehorn.96@gmail.com>
2019-11-17 11:35:58 +0100

    Update about.php

--- about.php
+++ about.php
@@ -24,21 +24,19 @@
             <button type="button" name="button" class="accordion">Porträtt</button>
             <div class="panel">
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Ett bra porträtt öppnar nya dörrar oavsett om det
                 </p>
             </div>
             <button type="button" name="button" class="accordion">Analog</button>
             <div class="panel">
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Vissa ögonblick är så unika att det bara måste fångas på ett lika unikt sätt, på 35mm film.
+                    Ett äkta hantverk där varje bild räknas och måste vara perfekt. Analoga bilder har en känsla som man kan försöka efterlikna men aldrig riktigt fånga.
+                    Kontrastfyllda svartvita bilder, underbara färger och subtilt gryn får Er att sticka ut från mängden. Oavsett om det är till ett event, produkter eller porträtt.
+                    <br>
+                    <br>
+                    I och med processen om film innebär så är det lite dyrare och tar lite längre tid men jag kan garantera att det är värt det för det som är viktigast för Er.
                 </p>
-            </div>
-            <button type="button" name="button" class="accordion"><Analog>Event</Analog></button>
-            <div class="panel">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
         </div>
     </div>
     <script>
