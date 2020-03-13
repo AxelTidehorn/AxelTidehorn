6ebc66a05e950f47a7c2d2ca87adeeb4d1617868
Axel Tidehorn <axel.tidehorn.96@gmail.com>
2020-03-13 10:56:15 +0100

    Färgändring

--- index.php
+++ index.php
@@ -114,7 +114,7 @@
    </script>
 </main>
 <div class="wave-wrapper">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#111111" fill-opacity="1" d="M0,224L1440,64L1440,320L0,320Z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#263f2c" fill-opacity="1" d="M0,224L1440,64L1440,320L0,320Z"></path></svg>
 </div>
 <div class="contact-wrapper" id="contact">
     <p>
